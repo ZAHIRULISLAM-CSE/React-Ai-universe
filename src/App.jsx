e0b82b3cc76e8f27008c878061bfc18e157664ec
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-03-26 15:13:14 +0600

    see more button working done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AllData from './Components/AllData/AllData';
 
 const App = () => {
+  
   return (
     <div>
       <h1 className='text-3xl text-center mt-6 mb-5'>AI UNIVERSE HUB</h1>
--- src/Components/AllData/AllData.jsx
+++ src/Components/AllData/AllData.jsx
@@ -3,17 +3,27 @@ import SingleData from '../SingleData/SingleData';
 
 const AllData = () => {
    const [data,setData]=useState([])
+   const [limit,setLimit]=useState(false);
    useEffect(()=>{
         fetch("https://openapi.programming-hero.com/api/ai/tools")
         .then(response => response.json())
         .then( data => setData(data.data.tools))
    },[])
+
+   const showAllButton=()=>{
+    setLimit(true);
+   }
    
     return (
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6 mx-5'>
+        <div>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6 mb-6 mx-5'>
             {
-                data.map((singleData,index)=><SingleData data={singleData} key={index}></SingleData>)
+                data.slice(0,limit? 12: 6).map((singleData,index)=><SingleData data={singleData} key={index}></SingleData>)
             }
+        </div>
+        <div className='text-center'>
+        <button onClick={showAllButton} className="btn btn-secondary">See More</button>
+        </div>
         </div>
     );
 };
